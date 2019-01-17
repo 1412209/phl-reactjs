e46d9dcd3650457e1550f7f692003e94926fb789
Nguyễn Văn Huy <vanhuy.tali@gmail.com>
2019-01-17 13:16:34 +0700

    Basic Header

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "assets/css/style.css";
+import indexRoutes from "./routes/index.jsx";
+
+ReactDOM.render(
+	<Router>
+		<Switch>
+			{indexRoutes.map((prop, key) => {
+				return <Route path={prop.path} component={prop.component} key={key} />;
+			})}
+		</Switch>
+	</Router>
+	, document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
